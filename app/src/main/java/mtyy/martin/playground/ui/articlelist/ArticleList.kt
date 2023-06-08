2998db011a255b72d16de4fd8dbae09944bfb3ed
Martin M <7189966+mtyy@users.noreply.github.com>
2023-06-08 16:19:21 +0200

    Made reading flow nicer

--- app/src/main/java/mtyy/martin/playground/ui/articlelist/ArticleList.kt
+++ app/src/main/java/mtyy/martin/playground/ui/articlelist/ArticleList.kt
@@ -2,16 +2,19 @@ package mtyy.martin.playground.ui.articlelist
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import mtyy.martin.playground.data.network.Article
 
 @Composable
 fun ArticleList(articles: List<Article>) {
     Column {
         articles.forEach { article ->
-            Row {
+            Row(modifier = Modifier.padding(bottom = 10.dp)) {
                 Text(text = article.title)
             }
 
--- app/src/main/java/mtyy/martin/playground/ui/articlelist/ArticleListFragment.kt
+++ app/src/main/java/mtyy/martin/playground/ui/articlelist/ArticleListFragment.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import mtyy.martin.playground.ui.theme.PlaygroundTheme
 
@@ -25,9 +26,9 @@ class ArticleListFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                val articles = viewModel.articlesFlow.collectAsState() //todo withLifecycle
+                val articles by viewModel.articlesFlow.collectAsStateWithLifecycle()
                 PlaygroundTheme() {
-                    ArticleList(articles = articles.value) // todo property delegate
+                    ArticleList(articles = articles)
                 }
             }
         }
